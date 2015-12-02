0d3d04e52f42831abbc7a39b879d5a39dd69643f
tutujiaw <tujiaw@163.com>
2015-12-02 23:02:29 +0800

    search bar

--- WxArticle/Controller/KeySearchedViewController.swift
+++ WxArticle/Controller/KeySearchedViewController.swift
@@ -10,10 +10,21 @@
 import UIKit
 
 class KeySearchedViewController: UIViewController {
+ 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    var x = ["111", "222", "333", "4444"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        searchBar.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,3 +33,30 @@ class KeySearchedViewController: UIViewController {
         
     }
 }
+
+extension KeySearchedViewController: UITableViewDelegate, UITableViewDataSource {
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return x.count
+    }
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "SEARCH_CELL_ID"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellId)
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        }
+        cell?.textLabel?.text = x[indexPath.row]
+        return cell!
+    }
+}
+
+extension KeySearchedViewController: UISearchBarDelegate {
+    internal func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        self.navigationItem.title = searchBar.text
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
+
